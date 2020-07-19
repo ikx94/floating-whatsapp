58fa68577fb64ba3ce58b858113bb564be40a781
ikx94 <44823775+ikx94@users.noreply.github.com>
2020-07-19 19:42:09 -0300

    Update floating-wpp.js

--- floating-wpp.js
+++ floating-wpp.js
@@ -3,7 +3,7 @@
         var settings = $.extend({
             phone: '',
             message: '',
-            size: '72px',
+            size: '60px',
             backgroundColor: '#25D366',
             position: 'left',
             popupMessage: '',
